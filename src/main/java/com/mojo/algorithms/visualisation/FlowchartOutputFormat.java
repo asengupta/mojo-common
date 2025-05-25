4e4cdfffd08d19c62945ac85f86def3bc4aa1e14
avishek-sen-gupta <asgupta@thoughtworks.com>
2025-05-26 00:20:12 +0530

    Moving some Graphviz visualisation into common lib

---
+++ src/main/java/com/mojo/algorithms/visualisation/FlowchartOutputFormat.java
@@ -0,0 +1,7 @@
+package com.mojo.algorithms.visualisation;
+
+public record FlowchartOutputFormat(String extension, String lineStyle) {
+    public static FlowchartOutputFormat PNG = new FlowchartOutputFormat("png", "ortho");
+    public static FlowchartOutputFormat SVG = new FlowchartOutputFormat("svg", "ortho");
+    public static FlowchartOutputFormat MERMAID = new FlowchartOutputFormat("md", "");
+}
