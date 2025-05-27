73c23595d3da0f11cacd6b0fccc567bd5f8f91d7
avishek-sen-gupta <asgupta@thoughtworks.com>
2025-05-27 19:32:26 +0530

    More text manipulation things

--- src/main/java/com/mojo/algorithms/string/TranspilerNodeFormatter.java
+++ src/main/java/com/mojo/algorithms/string/TranspilerNodeFormatter.java
@@ -1,5 +1,7 @@
 package com.mojo.algorithms.string;
 
+import java.util.stream.Stream;
+
 import static org.apache.commons.lang3.StringUtils.truncate;
 
 public class TranspilerNodeFormatter {
@@ -15,5 +17,14 @@ public class TranspilerNodeFormatter {
 
     public String prettyShort(String s) {
         return prettyShort(s, 30);
+    }
+
+    public Stream<String> splitLinesStream(String s, int lineLength) {
+        if (s.length() < lineLength) return Stream.of(s);
+        return Stream.concat(Stream.of(truncate(s, lineLength)), splitLinesStream(s.substring(lineLength), lineLength));
+    }
+
+    public String splitLines(String s, int lineLength, String joinCharacter) {
+        return splitLinesStream(s, lineLength).reduce("",  (a, b) -> a + joinCharacter + b) + joinCharacter;
     }
 }
