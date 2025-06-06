09bfb4d392b6ca75ef1dd8e5f17280d107ef9539
avishek-sen-gupta <asgupta@thoughtworks.com>
2025-06-06 17:08:16 +0530

    Back to ortho for flowcharts

--- src/main/java/com/mojo/algorithms/visualisation/FlowchartOutputFormat.java
+++ src/main/java/com/mojo/algorithms/visualisation/FlowchartOutputFormat.java
@@ -2,6 +2,6 @@ package com.mojo.algorithms.visualisation;
 
 public record FlowchartOutputFormat(String extension, String lineStyle) {
     public static FlowchartOutputFormat PNG = new FlowchartOutputFormat("png", "ortho");
-    public static FlowchartOutputFormat SVG = new FlowchartOutputFormat("svg", "true");
+    public static FlowchartOutputFormat SVG = new FlowchartOutputFormat("svg", "ortho");
     public static FlowchartOutputFormat MERMAID = new FlowchartOutputFormat("md", "");
 }
